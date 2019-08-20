fc3c7e0c8338ecf855f575759a52f3a3f7f5185d
Abdayyoub96 <Abdal-rahman.m.ayyoub@st.ahu.edu.jo>
2019-08-20 18:39:46 +0300

    add main page

---
+++ where2go/where2go/Page/MainPage/Main_Ar.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace where2go.Page.MainPage
+{
+    public partial class Main_Ar : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
